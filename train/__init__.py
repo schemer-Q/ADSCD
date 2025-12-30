81443a06b36027e8ab9afdad1c2ea41879145dcd
schemer <1923520440@qq.com>
2025-12-30 15:28:02 +0800

    在 train 目录下添加 __init__.py

---
+++ train/__init__.py
@@ -0,0 +1,7 @@
+"""Package marker for the top-level `train` package.
+
+This file makes the `train` directory importable as a package (needed
+by imports like `train.vint_train.models...`).
+"""
+
+__all__ = []
